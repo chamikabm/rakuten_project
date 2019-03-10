3f8ed3aec79a7bee73d5d7fdc99b31720ab302e0
chamikabm <kasun.chamika@gmail.com>
2019-03-10 14:52:02 +0530

    introduce handlers for exceptions and success flows

--- rakuten_project_BE/src/main/java/com/rakuten/shop/index/controller/FileUploadController.java
+++ rakuten_project_BE/src/main/java/com/rakuten/shop/index/controller/FileUploadController.java
@@ -1,17 +1,20 @@
 package com.rakuten.shop.index.controller;
 
-import com.rakuten.shop.index.core.StorageFileNotFoundException;
+import com.rakuten.shop.index.core.*;
 import com.rakuten.shop.index.service.ShopValidatorService;
 import com.rakuten.shop.index.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.nio.file.Path;
+import java.util.List;
 
 @Controller
 public class FileUploadController {
@@ -28,16 +31,12 @@ public class FileUploadController {
 
 
     @PostMapping("/upload")
-    public String handleFileUpload(@RequestParam("file") MultipartFile file,
-                                   RedirectAttributes redirectAttributes)  throws Exception{
+    @ResponseBody
+    public List<Shop> handleFileUpload(@RequestParam("file") MultipartFile file) throws Exception {
         storageService.store(file);
-        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-        shopValidatorService.validateFileContent(fileName);
-        redirectAttributes.addFlashAttribute("message",
-                "You successfully uploaded " + file.getOriginalFilename() + "!");
-
-
-        return "redirect:/";
+        Path fileName = storageService.load(file.getOriginalFilename());
+        List<Shop> shops = shopValidatorService.validateFileContent(fileName);
+        return shops;
     }
 
     @ExceptionHandler(StorageFileNotFoundException.class)
@@ -45,8 +44,25 @@ public class FileUploadController {
         return ResponseEntity.notFound().build();
     }
 
+    @ExceptionHandler(DuplicatedEntryException.class)
+    public ResponseEntity<?> handleDuplicatedEntryException(DuplicatedEntryException exc) {
+
+        return ResponseEntity.badRequest().body(exc.getMessage());
+    }
+
+    @ExceptionHandler(InvalidDateRangeException.class)
+    public ResponseEntity<?> handleInvalidDateRangeException(InvalidDateRangeException exc) {
+        return ResponseEntity.badRequest().body(exc.getMessage());
+    }
+
+    @ExceptionHandler(InvalidCSVContentException.class)
+    public ResponseEntity<?> handleInvalidCSVContentException(InvalidCSVContentException exc) {
+        return ResponseEntity.badRequest().body(exc.getMessage());
+    }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleStorageFileNotFound(Exception exc) {
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<?> handleGenericException(Exception exc) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exc);
     }
 }
