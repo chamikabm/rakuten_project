06a6ba1417a25708f4768d79da2114fc7c4148a8
chamikabm <kasun.chamika@gmail.com>
2019-03-10 14:46:05 +0530

    Refactored the Shop Entity class

--- rakuten_project_BE/src/main/java/com/rakuten/shop/index/core/Shop.java
+++ rakuten_project_BE/src/main/java/com/rakuten/shop/index/core/Shop.java
@@ -1,38 +1,65 @@
 package com.rakuten.shop.index.core;
 
 
-public class Shop {
-    int index;
-    int startDate;
-    int endDate;
+import java.util.Objects;
 
-    public Shop(int index, int startDate, int endDate) {
-        this.index = index;
+public class Shop {
+    Integer shopId;
+    Integer startDate;
+    Integer endDate;
+
+    public Shop(int shopId, int startDate, int endDate) {
+        this.shopId = shopId;
         this.startDate = startDate;
         this.endDate = endDate;
     }
 
-    public int getStartDate() {
+    public Integer getStartDate() {
         return startDate;
     }
 
-    public void setStartDate(int startDate) {
+    public void setStartDate(Integer startDate) {
         this.startDate = startDate;
     }
 
-    public int getEndDate() {
+    public Integer getEndDate() {
         return endDate;
     }
 
-    public void setEndDate(int endDate) {
+    public void setEndDate(Integer endDate) {
         this.endDate = endDate;
     }
 
-    public int getIndex() {
-        return index;
+    public Integer getShopId() {
+        return shopId;
     }
 
-    public void setIndex(int index) {
-        this.index = index;
+    public void setShopId(Integer shopId) {
+        this.shopId = shopId;
+    }
+
+    @Override
+    public String toString() {
+        return "Shop{" +
+                "shopId=" + shopId +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Shop shop = (Shop) o;
+        return (Objects.equals(shopId,shop.shopId));
+    }
+
+    @Override
+    public int hashCode() {
+        int result = shopId != null ? shopId.hashCode() : 0;
+        result = 31 * result + (startDate != null ? startDate.hashCode() : 0);
+        result = 31 * result + (endDate != null ? endDate.hashCode() : 0);
+        return result;
     }
 }
